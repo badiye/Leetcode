76bfc0f3ac62cca15da296356cd6b68007c927cb
badiye <147994555+badiye@users.noreply.github.com>
2025-02-19 15:21:03 +0300

    Add files via upload

---
+++ BuildArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+class Solution {
+    public int[] buildArray(int[] nums) {
+        for(int i=0; i<nums.length; i++) {
+            j=nums[nums[i]];
+            
+            nums[i]= j;
+            
+        }
+        return ans;
+    }
+}
