9a8d9b524fbbe0bc991325b17cf72fe3eaa6c166
badiye <147994555+badiye@users.noreply.github.com>
2025-02-26 20:26:14 +0300

    Time: 7 ms (48.05%) | Memory: 45.5 MB (52.02%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -1,34 +1,31 @@
-import java.util.*;
-
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        List<Integer> newAsteroids = new ArrayList<>();
-        int i = 0;       
-        while (i < asteroids.length) {
-            if (newAsteroids.isEmpty() || asteroids[i] > 0) {
-                newAsteroids.add(asteroids[i]);
-                i++;
-            } else {
-                int top = newAsteroids.get(newAsteroids.size() - 1);
-                if (top > 0 && asteroids[i] < 0) {
-                    if (Math.abs(top) > Math.abs(asteroids[i])) {
-                        i++;
-                    } else if (Math.abs(top) < Math.abs(asteroids[i])) {
-                        newAsteroids.remove(newAsteroids.size() - 1);
-                    } else {
-                        newAsteroids.remove(newAsteroids.size() - 1);
-                        i++;
-                    }
+        Stack<Integer> stack = new Stack<>();        
+        for (int asteroid : asteroids) {
+            boolean exploded = false;
+            while (!stack.isEmpty() && asteroid < 0 && stack.peek() > 0) {
+                if (stack.peek() < Math.abs(asteroid)) {
+                    stack.pop();
+                    continue;
+                } else if (stack.peek() == Math.abs(asteroid)) {
+                    stack.pop();
+                    exploded = true;
+                    break;
                 } else {
-                    newAsteroids.add(asteroids[i]);
-                    i++;
+                    exploded = true;
+                    break;
                 }
             }
+            if (!exploded) {
+                stack.push(asteroid);
+            }
         }
-        int[] result = new int[newAsteroids.size()];
-        for (int j = 0; j < newAsteroids.size(); j++) {
-            result[j] = newAsteroids.get(j);
+        int[] result = new int[stack.size()];
+        int i = 0;
+        for (int asteroid : stack) {
+            result[i++] = asteroid;
         }
+        
         return result;
     }
 }
